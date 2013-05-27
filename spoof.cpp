00ce033fff1bc95171b44112ebd59bb7f0a8ff30
demonicalmonk <demonicalm@gmail.com>
2013-05-27 20:15:10 +0200

    code reformat

--- spoof.cpp
+++ spoof.cpp
@@ -6,167 +6,168 @@
 #include <shlwapi.h>
 #include <string.h>
 #include <map>
+
 #include "spoof.h"
 #include "sha1.h"
 
 std::map<int, key_t> vdf_keys;
 
 void dump_key(key_t key) {
-	char hexstring[41];
-	sha1::toHexString(key, hexstring);
-	std::cout << hexstring;
+    char hexstring[41];
+    sha1::toHexString(key, hexstring);
+    std::cout << hexstring;
 }
 
 std::string strip_name(char* name) {
-	std::string str(name);
-	unsigned int pos = str.rfind("\\");
-	if (pos != std::string::npos) {
-		str = str.substr(pos+1);
-	}
+    std::string str(name);
+    unsigned int pos = str.rfind("\\");
+    if (pos != std::string::npos) {
+        str = str.substr(pos+1);
+    }
     std::transform(str.begin(), str.end(), str.begin(), ::toupper);
-	return str;
+    return str;
 }
 
 void crypt_buffer(char* buffer, unsigned int len,
                   key_t key, unsigned int initpos) {
-	unsigned int pos = initpos % KEY_LEN;
-	for(unsigned int i = 0; i < len; i++) {
-		*buffer ^= key[pos];
-		buffer++;
-		pos++;
-		if (pos >= KEY_LEN) {
-			pos = 0;
-		}
-	}
+    unsigned int pos = initpos % KEY_LEN;
+    for(unsigned int i = 0; i < len; i++) {
+        *buffer ^= key[pos];
+        buffer++;
+        pos++;
+        if (pos >= KEY_LEN) {
+            pos = 0;
+        }
+    }
 }
 
 bool check_key(int handle, key_t key) {
-	unsigned char key_hash[KEY_LEN];
-	unsigned char file_hash[KEY_LEN];
-	if (vdf_fread(handle, (char*)file_hash, KEY_LEN) != KEY_LEN) {
-		#ifdef DEBUG_FEATURES
-		std::cout << "-> KEY CHECK FAILED! (1)" << std::endl;
-		#endif
-		return false;
-	}
-	sha1::calc(key, KEY_LEN, (key_t)key_hash);
-	if (memcmp((const char*)key_hash, (const char*)file_hash, KEY_LEN) != 0) {
-		#ifdef DEBUG_FEATURES
-		std::cout << "-> KEY CHECK FAILED! (2)" << std::endl;
-		#endif
-		return false;
-	}
-	return true;
+    unsigned char key_hash[KEY_LEN];
+    unsigned char file_hash[KEY_LEN];
+    if (vdf_fread(handle, (char*)file_hash, KEY_LEN) != KEY_LEN) {
+        #ifdef DEBUG_FEATURES
+        std::cout << "-> KEY CHECK FAILED! (1)" << std::endl;
+        #endif
+        return false;
+    }
+    sha1::calc(key, KEY_LEN, (key_t)key_hash);
+    if (memcmp((const char*)key_hash, (const char*)file_hash, KEY_LEN) != 0) {
+        #ifdef DEBUG_FEATURES
+        std::cout << "-> KEY CHECK FAILED! (2)" << std::endl;
+        #endif
+        return false;
+    }
+    return true;
 }
 
 int DLL_EXPORT hook_vdf_fopen(char* name, int mode) {
-	int handle = vdf_fopen(name, mode);
-	
-	if (handle >= 0) {
-		// check for encrypted file header
-		unsigned int magic;
-		if (vdf_fread(handle, (char*)&magic, 4) == 4 && magic == MAGIC) {
-			std::string base_name = strip_name(name);
-			key_t key = new unsigned char[KEY_LEN];
-			#ifdef DEBUG_FEATURES
-			std::cout << "decrypting " << base_name << std::endl;
-			#endif
-			sha1::calc(base_name.c_str(), base_name.size(), key);
-			if (!check_key(handle, key)) {
-				#ifdef DEBUG_FEATURES
-				std::string error_text = "Failed to decrypt file!\n";
-				error_text += name;
-				error_text += "\nKey mismatch!";
-				MessageBox(NULL, error_text.c_str(), "GVC", MB_ICONERROR);
-				#endif
-				return -1;
-			}
-			#ifdef DEBUG_FEATURES
-			std::cout << "-> with key: ";
-			dump_key(key);
-			std::cout << std::endl;
-			#endif
-			vdf_keys.insert(std::pair<int, key_t>(handle, key));
-		} else {
-			#ifdef DEBUG_FEATURES
-			std::cout << "open " << name << std::endl;
-			#endif
-			vdf_fseek(handle, 0);
-		}
-	}
+    int handle = vdf_fopen(name, mode);
 
-	return handle;
+    if (handle >= 0) {
+        // check for encrypted file header
+        unsigned int magic;
+        if (vdf_fread(handle, (char*)&magic, 4) == 4 && magic == MAGIC) {
+            std::string base_name = strip_name(name);
+            key_t key = new unsigned char[KEY_LEN];
+            #ifdef DEBUG_FEATURES
+            std::cout << "decrypting " << base_name << std::endl;
+            #endif
+            sha1::calc(base_name.c_str(), base_name.size(), key);
+            if (!check_key(handle, key)) {
+                #ifdef DEBUG_FEATURES
+                std::string error_text = "Failed to decrypt file!\n";
+                error_text += name;
+                error_text += "\nKey mismatch!";
+                MessageBox(NULL, error_text.c_str(), "GVC", MB_ICONERROR);
+                #endif
+                return -1;
+            }
+            #ifdef DEBUG_FEATURES
+            std::cout << "-> with key: ";
+            dump_key(key);
+            std::cout << std::endl;
+            #endif
+            vdf_keys.insert(std::pair<int, key_t>(handle, key));
+        } else {
+            #ifdef DEBUG_FEATURES
+            std::cout << "open " << name << std::endl;
+            #endif
+            vdf_fseek(handle, 0);
+        }
+    }
+
+    return handle;
 }
 
 int DLL_EXPORT hook_vdf_fseek(int handle, long offset) {
-	if (vdf_keys.find(handle) != vdf_keys.end()) {
-		offset += 4+KEY_LEN;
-	}
-	
-	return vdf_fseek(handle, offset);
+    if (vdf_keys.find(handle) != vdf_keys.end()) {
+        offset += 4+KEY_LEN;
+    }
+
+    return vdf_fseek(handle, offset);
 }
 
 int DLL_EXPORT hook_vdf_fclose(int handle) {
-	if (vdf_keys.find(handle) != vdf_keys.end()) {
-		delete[] vdf_keys[handle];
-		vdf_keys.erase(handle);
-	}
-	
-	return vdf_fclose(handle);
+    if (vdf_keys.find(handle) != vdf_keys.end()) {
+        delete[] vdf_keys[handle];
+        vdf_keys.erase(handle);
+    }
+
+    return vdf_fclose(handle);
 }
 
 long DLL_EXPORT hook_vdf_fread(int handle, char* buffer, long len) {
-	long result = vdf_fread(handle, buffer, len);
-	if (vdf_keys.find(handle) != vdf_keys.end()) {
-		long offset = vdf_ftell(handle)-len-4-KEY_LEN;
-		crypt_buffer(buffer, len, vdf_keys[handle], offset);
-	}
-	return result;
+    long result = vdf_fread(handle, buffer, len);
+    if (vdf_keys.find(handle) != vdf_keys.end()) {
+        long offset = vdf_ftell(handle)-len-4-KEY_LEN;
+        crypt_buffer(buffer, len, vdf_keys[handle], offset);
+    }
+    return result;
 }
 
 long DLL_EXPORT hook_vdf_ftell(int handle) {
-	long result = vdf_ftell(handle);
-	
-	if (vdf_keys.find(handle) != vdf_keys.end()) {
-		result -= 4+KEY_LEN;
-	}
-	
-	return result;
+    long result = vdf_ftell(handle);
+
+    if (vdf_keys.find(handle) != vdf_keys.end()) {
+        result -= 4+KEY_LEN;
+    }
+
+    return result;
 }
 
 extern "C" BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
-	if (fdwReason != DLL_PROCESS_ATTACH) {
-		return 1;
-	}
-	
-	#ifdef DEBUG_FEATURES
-	AllocConsole();
+    if (fdwReason != DLL_PROCESS_ATTACH) {
+        return 1;
+    }
+
+    #ifdef DEBUG_FEATURES
+    AllocConsole();
     freopen("CONOUT$", "w", stdout);
     std::cout.sync_with_stdio();
-	#endif
+    #endif
 
-	LPWSTR *szArglist;
-	int nArgs;
-	
-	szArglist = CommandLineToArgvW(GetCommandLineW(), &nArgs);
-	if (szArglist == NULL) {
-		wprintf(L"CommandLineToArgvW failed\n");
-		return 0;
-	} else {
-		for(int i = 0; i<nArgs; i++) {
-			if (StrCmpW(szArglist[i], L"-gtools") == 0) {
-				if (LoadLibrary("gtools.dll") == NULL) {
-					return 0;
-				}
-			} else if (StrCmpW(szArglist[i], L"-gtools-dev") == 0) {
-				if (LoadLibrary("gtools-dev.dll") == NULL) {
-					return 0;
-				}
-			}
-		}
-	}
+    LPWSTR* szArglist;
+    int nArgs;
 
-	LocalFree(szArglist);
+    szArglist = CommandLineToArgvW(GetCommandLineW(), &nArgs);
+    if (szArglist == NULL) {
+        wprintf(L"CommandLineToArgvW failed\n");
+        return 0;
+    } else {
+        for(int i = 0; i<nArgs; i++) {
+            if (StrCmpW(szArglist[i], L"-gtools") == 0) {
+                if (LoadLibrary("gtools.dll") == NULL) {
+                    return 0;
+                }
+            } else if (StrCmpW(szArglist[i], L"-gtools-dev") == 0) {
+                if (LoadLibrary("gtools-dev.dll") == NULL) {
+                    return 0;
+                }
+            }
+        }
+    }
 
-	return 1;
+    LocalFree(szArglist);
+
+    return 1;
 }
